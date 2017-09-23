cc4b613819130a74d9bde122019b9b52ed6db8c7
abhijitmallik <abhijitmallik84@gmail.com>
2017-09-24 01:03:47 +0530

    set up front end

--- public/javascripts/init.js
+++ public/javascripts/init.js
@@ -2,11 +2,11 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import {createStore,applyMiddleware} from 'redux';
 import {Provider} from 'react-redux';
-import ReduxPromise from 'redux-promise';
+import thunk from 'redux-thunk';
 import App from './component/app';
 import reducers from './reducers';
 
-const createStoreWithMiddleware = applyMiddleware(ReduxPromise)(createStore);
+const createStoreWithMiddleware = applyMiddleware(thunk)(createStore);
 ReactDOM.render( 
   <Provider store={createStoreWithMiddleware(reducers)}>
     <App  />
