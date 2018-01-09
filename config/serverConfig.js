8c23edcb658e63b0cdfb71733e4853b7b45831b0
abhijitmallik <abhijitmallik84@gmail.com>
2018-01-09 20:57:06 +0530

    changed the cong

--- config/serverConfig.js
+++ config/serverConfig.js
@@ -4,7 +4,8 @@ module.exports = {
 	},
 	dbURL:'mongodb://knockthedoor:laptoppc84@ds231245.mlab.com:31245/knockthedoor',
 	redis:{
-		host: process.env.REDISCLOUD_URL,
-		port:10428
+		host: process.env.REDISCLOUD_ONYX_URL,
+		port:10428,
+		no_ready_check: true
 	}
 }
