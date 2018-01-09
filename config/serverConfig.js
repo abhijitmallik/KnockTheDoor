8c3318817a58a90beccec4d5edc9428b64a4cd34
abhijitmallik <abhijitmallik84@gmail.com>
2018-01-09 18:52:31 +0530

    changed the port

--- config/serverConfig.js
+++ config/serverConfig.js
@@ -3,8 +3,8 @@ module.exports = {
 		port:[3000]
 	},
 	dbURL:'mongodb://knockthedoor:laptoppc84@ds231245.mlab.com:31245/knockthedoor',
-	redis:{ 
-		host: 'https://bankerexam.herokuapp.com',
+	redis:{
+		host: '127.0.0.1',
         port: 6379
 	}
 }
