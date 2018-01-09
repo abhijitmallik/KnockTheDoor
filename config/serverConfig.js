783eb33b798458a5d70455b9efb23dc418bfc8ef
abhijitmallik <abhijitmallik84@gmail.com>
2018-01-09 19:23:26 +0530

    changed the url redis

--- config/serverConfig.js
+++ config/serverConfig.js
@@ -4,7 +4,6 @@ module.exports = {
 	},
 	dbURL:'mongodb://knockthedoor:laptoppc84@ds231245.mlab.com:31245/knockthedoor',
 	redis:{
-		host: process.env.REDISCLOUD_URL,
-        port: 17586
+		host: process.env.REDISCLOUD_URL
 	}
 }
