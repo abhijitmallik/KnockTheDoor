34d619323ddd93cdf5e41bb1281f363fb31bd2b5
abhijitmallik <abhijitmallik84@gmail.com>
2018-01-09 19:19:15 +0530

    added redis cloude

--- config/serverConfig.js
+++ config/serverConfig.js
@@ -5,6 +5,6 @@ module.exports = {
 	dbURL:'mongodb://knockthedoor:laptoppc84@ds231245.mlab.com:31245/knockthedoor',
 	redis:{
 		host: process.env.REDISCLOUD_URL,
-        port: 6379
+        port: 17586
 	}
 }
