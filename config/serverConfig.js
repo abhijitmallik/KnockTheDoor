431963d554ceb6c0e084df4bc493126f99e0b118
abhijitmallik <abhijitmallik84@gmail.com>
2018-01-09 18:58:13 +0530

    changed the heroku url

--- config/serverConfig.js
+++ config/serverConfig.js
@@ -4,7 +4,7 @@ module.exports = {
 	},
 	dbURL:'mongodb://knockthedoor:laptoppc84@ds231245.mlab.com:31245/knockthedoor',
 	redis:{
-		host: '127.0.0.1',
+		host: process.env.REDISCLOUD_URL,
         port: 6379
 	}
 }
