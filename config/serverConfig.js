cc933b0fb3744f0c3ee9a278e8d86c66cbd25095
abhijitmallik <abhijitmallik84@gmail.com>
2018-01-09 17:45:18 +0530

    chnaged the redis url

--- config/serverConfig.js
+++ config/serverConfig.js
@@ -4,7 +4,7 @@ module.exports = {
 	},
 	dbURL:'mongodb://knockthedoor:laptoppc84@ds231245.mlab.com:31245/knockthedoor',
 	redis:{
-		host: 'bankerexam.herokuapp.com',
+		host: 'https://bankerexam.herokuapp.com',
         port: 6379
 	}
 }
