df89f4a87a7f8b2cbf8d3cdc99012774ef1a2dcd
abhijitmallik <abhijitmallik84@gmail.com>
2018-01-10 16:52:39 +0530

    changed the configuration

--- production/production.js
+++ production/production.js
@@ -56150,7 +56150,29 @@ var WhiteBoardComponent = function (_Component) {
       /*let configuration = {
         "iceServers": [{ "url": "stun:stun.l.google.com:19302" }]
       };*/
-      var configuration = { 'iceServers': ["stun.l.google.com:19302", "stun1.l.google.com:19302", "stun2.l.google.com:19302", "stun3.l.google.com:19302", "stun4.l.google.com:19302", "stun.ekiga.net", "stun.ideasip.com", "stun.rixtelecom.se", "stun.schlund.de", "stun.stunprotocol.org:3478", "stun.voiparound.com", "stun.voipbuster.com", "stun.voipstunt.com", "stun.voxgratia.org"] };
+      var configuration = { 'iceServers': [{
+          'url': 'stun:stun.l.google.com:19302'
+        }, {
+          'url': 'stun:stun1.l.google.com:19302'
+        }, {
+          'url': 'stun:stun2.l.google.com:19302'
+        }, {
+          'url': 'stun:stun3.l.google.com:19302'
+        }, {
+          'url': 'stun:stun4.l.google.com:19302'
+        }, {
+          'url': 'stun:stun.ekiga.net'
+        }, {
+          'url': 'stun:stun.ideasip.com'
+        }, {
+          'url': 'stun:stun.rixtelecom.se'
+        }, {
+          'url': 'stun:stun.schlund.de'
+        }, {
+          'url': 'stun:stun.stunprotocol.org:3478'
+        }, {
+          'url': 'stun:stun.voiparound.com'
+        }] };
 
       rtcConnection = new RTCPeerConnection(configuration);
       rtcConnection.addStream(stream);
